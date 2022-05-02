c123c495ab464308a1afd16e080d7d248e2f6894
Alexander Berntsson <alexande.berntsson@gmail.com>
2022-05-02 20:37:41 +0200

    Scrolling output window.

--- src/output_window.rs
+++ src/output_window.rs
@@ -11,21 +11,47 @@ lazy_static! {
 }
 
 pub struct OutputWindow {
-    data: String,
+
+    // Content of all lines received from arduino. Excludes '\n'.
+    lines: Vec<String>,
+    
     pub window: Window,
 }
 
 impl OutputWindow {
     pub fn new(window: Window) -> Self {
         OutputWindow {
-            data: String::new(),
+            lines: vec![String::new()],
             window,
         }
     }
 
+    pub fn reprint_lines(&mut self) {
+        self.window.mv(0, 0);
+        let height = self.window.get_max_y();
+        let len = self.lines.len();
+        for line in &self.lines[0i32.max(len as i32-height-1) as usize..] {
+            self.window.clrtoeol();
+            self.window.addstr(line);
+            self.window.addch('\n');
+        }
+    }
+
     pub fn add_data(&mut self, s: &str) {
-        self.window.addnstr(&s, s.len());
-        self.data.push_str(s);
+        let height = self.window.get_max_y();
+        for ch in s.chars() {
+            if ch == '\n' {
+                let cur_y = self.window.get_cur_y();
+                self.lines.push(String::new());
+                if cur_y == height-1 {
+                    self.reprint_lines();
+                }
+            } else {
+                self.lines.last_mut().unwrap().push(ch);
+            }
+            self.window.addch(ch);
+
+        }
         self.window.refresh();
     }
     
